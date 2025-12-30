52b30a6b991dc4bf09d433ea300d0bccfc6bd1dc
mho22 <yannick@capsules.codes>
2025-12-30 13:27:18 +0100

    Configure PHP.wasm Dockerfile to include shared libraries

--- packages/php-wasm/compile/shared/build.js
+++ packages/php-wasm/compile/shared/build.js
@@ -89,8 +89,6 @@ await asyncSpawn(
 	{ cwd: path.dirname(sourceDir), stdio: 'inherit' }
 );
 
-const version = args['PHP_VERSION'].replace('.', '_');
-
 // Store the shared library in output directories
 await asyncSpawn(
 	'docker',
@@ -106,9 +104,9 @@ await asyncSpawn(
 		// they don't work without running cp through shell.
 		'sh',
 		'-c',
-		`rm -rf /output/extensions/${library}/${version} && \
-			mkdir -p /output/extensions/${library}/${version} && \
-			cp -rf /root/${library}/modules/* /output/extensions/${library}/${version}`,
+		`rm -rf /output/extensions/${library} && \
+			mkdir -p /output/extensions/${library} && \
+			cp -rf /root/${library}/modules/* /output/extensions/${library}`,
 	],
 	{ cwd: path.dirname(sourceDir), stdio: 'inherit' }
 );
